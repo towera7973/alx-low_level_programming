5a20837ec7d3d1e264bf5d10e215d2fa400e5ed7
towera7973 <towera7973@gmail.com>
2023-02-16 11:37:01 -0800

    repeat this

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
 #include <unistd.h>
 
 /**
- *let the  main  function  Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *   followed by a new line, to standard error.
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
  *
- * let it retuen 1 instead of 0 
- * the function to use is write 
+ * Return: Always 1.
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
 
 	return (1);
 }
