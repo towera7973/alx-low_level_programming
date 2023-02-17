6b6175a86a53e467bceb8e00dcf82e441aaf34b1
towera7973 <towera7973@gmail.com>
2023-02-17 15:09:51 -0800

    hey

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+/*
+ * Towera mndoli
+ */
+#include <stdio.h>
+
+/**
+ * main - printa program that prints all possible
+ * different combinations of two digits.
+ * Numbers must be separated by ,, followed by a space
+ *
+ * * Return: Always 0.
+ */
+int main(void)
+{
+	int number1, number2;
+
+	for (number1 = 0; number1 < 10; number1++)
+	{
+		for (number2 = 0; number2 < 10; number2++)
+		{
+			putchar((number1 % 10) + '0');
+			putchar((number2 % 10) + '0');
+
+			if (number1 == 9 && number2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
