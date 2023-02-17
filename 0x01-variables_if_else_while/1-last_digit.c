b53910b5a78a02919e1f0b57a3e6ab4b5904ab0b
towera7973 <towera7973@gmail.com>
2023-02-17 06:35:08 -0800

    betty is a great tool

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,25 +3,34 @@
 #include <stdio.h>
 
 /**
- * main - assign a random number to the variable n each time it is executed
- *The var  n will store a dif  value every time you run this program
- *The output of the program should be:
- *The string Last digit of, followed by n,followed by
+ * main - Prints the last digit of a randomly generated number
+ *        and whether it is greater than 5, less than 6, or 0.
  *
- *Return: Always 0.
+ * Return: Always 0.
  */
-
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n % 10 > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+
+	if ((n % 10) > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n",
+			n, n % 10);
+	}
 	else if ((n % 10) < 6 && (n % 10) != 0)
-		printf("the last digit of %d is %d and is less that 6 and not 0\n", n, n % 10);
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+			n, n % 10);
+	}
 	else
-		 printf("Last digit of %d is %d and is zero\n", n, n % 10);
+	{
+		printf("Last digit of %d is %d and is 0\n",
+			n, n % 10);
+	}
+
 	return (0);
+
 }
