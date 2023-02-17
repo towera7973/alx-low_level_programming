3652585584b34af9c61af10d82b74450fdf5586b
towera7973 <towera7973@gmail.com>
2023-02-17 07:50:08 -0800

    print all letters except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+/*
+ * By Towera mndoli
+ */
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase  followed by a new line.
+ * Print all the letters except q and e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
