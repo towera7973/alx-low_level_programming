c757ba263f07f855f97b6c196a81adb00ef1cba3
towera7973 <towera7973@gmail.com>
2023-02-17 07:23:58 -0800

    printing letters in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+/*
+ * By Towera Mndoli
+ *
+ */
+#include <stdio.h>
+
+/**
+ * main - Prints A-Z alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
