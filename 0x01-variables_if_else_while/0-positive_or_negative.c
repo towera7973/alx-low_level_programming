de41233fede2daa93bdfdacee5fd3fb75227350e
towera7973 <towera7973@gmail.com>
2023-02-18 12:17:35 -0800

    positive or nagativr random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,10 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - assign a random # to the variable n each time it is executed.
- *  Complete the source code in order to print whether the
- *  number stored in the variable n is + or -.
- *Return: Always 0.
+ * main -  assign a random number to the variable n each time it is executed
+ *
+ *Return: always 0;
  */
 int main(void)
 {
@@ -13,14 +12,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-	if (n < 0)
-
-		printf("%d is negative\n", n);
-	else if (n > 0)
-
+	if (n > 0)
 		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
 	else
-		printf("%d is zero\n", n);
+		printf("%d is 0\n", n);
+
 	return (0);
 }
