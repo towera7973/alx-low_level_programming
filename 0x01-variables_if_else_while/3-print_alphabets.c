052be0a3b675ff0dc92c1fb0d298e8d1d263c9e0
towera7973 <towera7973@gmail.com>
2023-02-18 13:09:28 -0800

    numbers 0 to 9

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lower, and then  upper case with end line.
+ * main - alphabet in lowercase, and then in uppercase.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char letter;
+	char l;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
+	for (l = 'a'; l <= 'z'; l++)
+		putchar(l);
 
-	for (letter = 'A'; letter <= 'Z'; letter++)
-		putchar(letter);
+	for (l = 'A'; l <= 'Z'; l++)
+		putchar(l);
 
 	putchar('\n');
 
 	return (0);
 }
+
