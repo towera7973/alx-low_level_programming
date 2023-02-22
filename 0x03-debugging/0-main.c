fe0c623402a7dd7c035bb26ef913de1054c1c877
towera7973 <towera7973@gmail.com>
2023-02-22 13:22:31 -0800

    Printing positive and negative numbers

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * main -  prints negative ot positive num
+  *
+  * Return: always 0;
+  */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	positive_or_negative(n);
+
+	return (0);
+}
+
