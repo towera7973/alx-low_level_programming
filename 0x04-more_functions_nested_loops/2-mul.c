c75dda55b61ef7c6c8123b543e23ff704ed72ca8
towera7973 <towera7973@gmail.com>
2023-02-23 10:40:19 -0800

    the multiple of a and b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - muiltiplies two numbers
+ *@a : integer 1
+ *@b : integer 2
+ *Return: always 0;
+ */
+
+int mul(int a, int b)
+{
+	a = 2;
+
+	b = 4;
+
+	printf("The muiltiply of %d and %d is :\n", a, b);
+	return (a * b);
+}
