b322cd37ff7c18410b3b3edbcf90d1fb500d6135
towera7973 <towera7973@gmail.com>
2023-03-01 09:33:44 -0800

    fixing errors

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_number - Write a function that prints an intege
+ *You can only use _putchar function to print
+ * You are not allowed to use long
+ * @n:
+ * You are not allowed to use arrays or pointers
+ * You are not allowed to hard-code special values
+ * Return: always 0
+ */
+
+void print_number(int n)
+{
+	n = 19;
+
+	_putchar("n \n");
+}
