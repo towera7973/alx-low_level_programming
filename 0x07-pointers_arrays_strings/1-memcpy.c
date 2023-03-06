a0e512cb2dba7a7030b334dce790ac38a0b649d7
towera7973 <towera7973@gmail.com>
2023-03-06 01:51:42 -0800

    memory copy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * _memset - Entry point
- * @s: pointed destination
- * @b: constant byte
- * @n: bytes
+ * _memcpy - Entry point
+ * @dest: input
+ * @src: input
+ * @n: input
  * Return: Always 0 (Success)
  */
-char *_memset(char *s, char b, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-		s[i] = b;
-	return (s);
+		dest[i] = src[i];
+
+	return (dest);
 }
