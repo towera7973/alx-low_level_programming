00416bf6ef38f51a324404c6508986c208e73934
towera7973 <towera7973@gmail.com>
2023-03-08 10:48:39 -0800

    corrections

--- 0x08-recursion/towera.h
+++ 0x08-recursion/towera.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 int _isupper(int c);
@@ -45,9 +47,12 @@ void set_string(char **s, char *to);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
+int pal_check(char *s, int l)
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+
+#endif
