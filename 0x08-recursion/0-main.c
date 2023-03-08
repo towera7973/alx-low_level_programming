8574decfdbddebd100d0747d6eceae42de24d353
towera7973 <towera7973@gmail.com>
2023-03-08 05:48:38 -0800

    recursion is fun

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,12 @@
+#include "towera.h"
+
+/**
+ * main - check the code for alx School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    _puts_recursion("Betty main");
+    return (0);
+}
---
+++ 0x08-recursion/100-main.c
@@ -0,0 +1,27 @@
+#include "towera.h"
+#include <stdio.h>
+
+/**
+ * main - check the code alx  School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = wildcmp("main.c", "*.c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "m*a*i*n*.*c*");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "main.c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "m*c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "ma********************************c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "*");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "***");
+    printf("%d\n", r);
+-- INSERT --        
