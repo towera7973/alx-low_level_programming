bcfed12ad9b79f39f7fba092ad29807f76e6a489
towera7973 <towera7973@gmail.com>
2023-03-09 12:28:27 -0800

    correction

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,17 @@
 #include "towera.h"
+
 /**
- * _puts_recursion - puts a string to stdout recursively
- *
- * @s: string to put to stdout
- *
- * Return: always void
+ * _puts_recursion - Print a string
+ * @s: string
  */
+
 void _puts_recursion(char *s)
 {
-	if (*s == '\0') /* if s points to null char */
-	{
+	if (*s == '\0')
 		_putchar('\n');
-		return;
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
 	}
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
 }
