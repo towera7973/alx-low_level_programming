ecbe2fe93203ba826a808d07a423f6863c1f17c0
towera7973 <towera7973@gmail.com>
2023-04-03 08:05:08 -0700

    fix it

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,30 +1,27 @@
 /*
- * File: 7-get_nodeint.c
- * Auth: Towera Mndoli
+ * File: 8-sum_listint.c
+ * Auth:Towera MNDOLI
  */
 
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - Locates a given node of
- *                        a listint_t linked list.
+ * sum_listint - Calculates the sum of all the
+ *               data (n) of a listint_t list.
  * @head: A pointer to the head of the listint_t list.
- * @index: The index of the node to locate - indices start at 0.
  *
- * Return: If the node does not exist - NULL.
- *         Otherwise - the located node.
+ * Return: If the list is empty - 0.
+ *         Otherwise - the sum of all the data.
  */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+int sum_listint(listint_t *head)
 {
-	unsigned int node;
+	int sum = 0;
 
-	for (node = 0; node < index; node++)
+	while (head)
 	{
-		if (head == NULL)
-			return (NULL);
-
+		sum += head->n;
 		head = head->next;
 	}
 
-	return (head);
+	return (sum);
 }
