8a19ca1d59db6faf4600806c9137e62298ac19ac
towera7973 <towera7973@gmail.com>
2023-04-10 13:36:42 -0700

    all

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,24 +15,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int openingfile, writtingfile, length = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (length = 0; text_content[length];)
+			length++;
 	}
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
+	openingfile = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	writtingfile = write(openingfile, text_content, length);
 
-	if (o == -1 || w == -1)
+	if (openingfile == -1 || writtingfile == -1)
 		return (-1);
 
-	close(o);
+	close(openingfile);
 
 	return (1);
 }
