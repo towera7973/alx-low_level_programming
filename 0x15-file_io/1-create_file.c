1d56a0f9ad0da7de274c3cf11be0c6e857f8151c
towera7973 <towera7973@gmail.com>
2023-11-22 11:38:07 -0800

    fix2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,12 +27,10 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	openingfile = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (openingfile == -1)
-		return(-1);
-
 	writtingfile = write(openingfile, text_content, length);
-	if (writtingfile == -1)
-		return(-1);
+
+	if (openingfile == -1 || writtingfile == -1)
+		return (-1);
 
 	close(openingfile);
 
