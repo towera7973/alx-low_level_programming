4db4e2bc1599cbe02269ff40209bf0dfb5579df6
towera7973 <@towera7973@gmail.com@>
2023-11-23 03:55:38 -0800

    good code

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,9 +10,10 @@
  * @n: The number to be printed in binary.
  */
 void print_binary(unsigned long int n)
-{
-	if (n > 1)
-		print_binary(n >> 1);
-
-	_putchar((n & 1) + '0');
+{	if (n == 0) {
+        _putchar('0');
+        return;
+    }
+    print_binary(n >> 1);
+    _putchar((n & 1) + '0');
 }
