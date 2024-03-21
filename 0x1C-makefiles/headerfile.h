eff560f3e1afa34f95b2930abf166dbd205296bd
towera7973 <towera7973@gmail.com>
2024-03-20 18:27:09 -0700

    first4

---
+++ 0x1C-makefiles/headerfile.h
@@ -0,0 +1,9 @@
+#ifndef __HEADERFILE_H__
+#define __HEADERFILE_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_school_function(void);
+
+#endif
---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,13 @@
+#include "headerfile.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 
+ */
+int main(void)
+{
+	print_school_function();
+	return (EXIT_SUCCESS);
+}
+/* School */
